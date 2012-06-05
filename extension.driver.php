f3e142c3a8460b57f78e008328b32f144d41b91b
Nick Baker <nbaker@internode.on.net>
2012-06-05 12:18:01 +0930

    Updated for Symphony 2.3

--- extension.driver.php
+++ extension.driver.php
@@ -2,19 +2,6 @@
 
 	Class Extension_XUACompatible extends Extension{
 
-		public function about(){
-			return array(
-				'name' => 'X-UA-Compatible Header',
-				'version' => '1.0',
-				'release-date' => '2011-10-21',
-				'author' => array(
-					'name' => 'Nick Baker',
-					'email' => 'nick@digitalsunshine.com.au'
-				),
-				'descripton' => 'Adds a X-UA-Compatible header to response to discourage IE from entering compatibility mode'
-			);
-		}
-
 		public function addXUACompatibleHeader($context = null){
 			Frontend::instance()->Page()->addHeaderToPage('X-UA-Compatible', 'IE=Edge,chrome=1');
 		}
